0fc1d1eabfb6affcce0d8efae7870fe97fc5ffe3
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-24 11:10:49 -0300

    Curso C# - Inferência

---
+++ CursoCSharp/Fundamentos/Inferencia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    class Inferencia {
+        public static void Executar() {
+            //Inferencia quando o c# reconhece o tipo apartir da atribuição que ele
+            // recebeu
+            var nome = "Adeilson";
+            Console.WriteLine(nome);
+
+            var idade = 28;
+            Console.WriteLine(idade);
+
+
+
+        }
+    }
+}
