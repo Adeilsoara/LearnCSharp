882cf8c8fffd6d133ed0cb9a6f046aef89580c9e
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-24 13:29:16 -0300

    Curso C# - Interpolação

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -11,6 +11,7 @@ namespace CursoCSharp {
                 {"Comentários CSharp - Fundamentos", Comentarios.Executar},
                 {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
                 {"Inferência - Fundamentos", Inferencia.Executar},
+                {"Interpolação - Fundamentos", Interpolacao.Executar},
             });
 
             central.SelecionarEExecutar();
