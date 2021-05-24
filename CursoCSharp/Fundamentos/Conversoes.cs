38d9e08d7095b6b11fc468117f0c5494fa31c2ec
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-24 15:51:42 -0300

    Curso C# - Conversões

---
+++ CursoCSharp/Fundamentos/Conversoes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    class Conversoes {
+        public static void Executar() {
+            int inteiro = 10;
+            double quebrado = inteiro;
+            Console.WriteLine(quebrado);
+
+            double nota = 8.9;
+            //conversão explícita, casting
+            int notaTruncada = (int)nota;
+            Console.WriteLine($"Nota truncada: {notaTruncada}");
+
+            Console.Write("Digite sua idade: ");
+            string idadeString = Console.ReadLine();
+            int idadeInteiro = int.Parse(idadeString);
+            Console.WriteLine("Idade Inserida: {0}", idadeInteiro);
+
+            idadeInteiro = Convert.ToInt32(idadeString);
+            Console.WriteLine("Resultado: {0}", idadeInteiro);
+
+            Console.Write("Digite o primeiro número: ");
+            string palavra = Console.ReadLine();
+            int numero;
+            int.TryParse(palavra, out numero);
+            Console.WriteLine("Resultado: {0}", numero);
+
+            Console.Write("Digite o segundo número: ");
+            int.TryParse(Console.ReadLine(), out int numero2);
+            Console.WriteLine("Resultado: {0}", numero2);
+
+
+        }
+    }
+}
