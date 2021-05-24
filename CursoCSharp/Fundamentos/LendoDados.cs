87677f4c4c7a7bf9e8fef2f5fa482a3f15235a4f
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-24 15:21:19 -0300

    Curso C# - Formatando Números

--- CursoCSharp/Fundamentos/LendoDados.cs
+++ CursoCSharp/Fundamentos/LendoDados.cs
@@ -5,13 +5,13 @@ using System.Globalization;
 namespace CursoCSharp.Fundamentos {
     class LendoDados {
         public static void Executar() {
-            Console.WriteLine("Qual o seu nome?: ");
+            Console.Write("Qual o seu nome?: ");
             string nome = Console.ReadLine();
 
-            Console.WriteLine("Qual é a sua idade?: ");
+            Console.Write("Qual é a sua idade?: ");
             int idade = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Qual seu salário?: ");
+            Console.Write("Qual seu salário?: ");
             double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             Console.WriteLine($"{nome} {idade} R$ {salario}");
