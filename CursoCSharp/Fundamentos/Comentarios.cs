53f64981a46de01eae4700e3395785bd18a46c0e
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-24 10:19:43 -0300

    Curso C# - Comentários em C#

--- CursoCSharp/Fundamentos/Comentarios.cs
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -6,9 +6,17 @@ using System.Threading.Tasks;
 
 namespace CursoCSharp.Fundamentos {
     class Comentarios {
-
+        /// <summary>
+        /// 
+        /// </summary>
         public static void Executar() {
 
+            //Ctrl + k + c ->> comenta o código
+            //ctrl + k + u -> descomenta o código
+            Console.WriteLine("Nem sempre comentar é necessário");
+            /*
+             Comentários de Múltiplas linhas
+             */
         }
     }
 }
