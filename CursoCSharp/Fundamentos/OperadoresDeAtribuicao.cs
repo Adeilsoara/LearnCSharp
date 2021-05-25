7a9c6c341e16f50fe63e1757be98d3bc1fc783bf
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-25 15:54:05 -0300

    Curso C# - Operadores de Atribuição

---
+++ CursoCSharp/Fundamentos/OperadoresDeAtribuicao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    class OperadoresDeAtribuicao {
+
+        public static void Executar() {
+            var num = 3;
+            num = 7;
+            num += 10;
+            num -= 10;
+            num *= 10;
+            num /= 10;
+
+            Console.WriteLine(num);
+
+            int a = 1;
+            int b = a;
+
+            a++;
+            b--;
+
+        }
+    }
+}
