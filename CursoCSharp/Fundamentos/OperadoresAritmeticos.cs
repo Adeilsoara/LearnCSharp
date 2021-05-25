acd6833798ce6e09ed05d81fdcf2954676f0a37c
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-25 14:08:59 -0300

    Curso C# - Operadores Aritmétcos

---
+++ CursoCSharp/Fundamentos/OperadoresAritmeticos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    class OperadoresAritmeticos {
+        public static void Executar() {
+            var preco = 1000.0;
+            var imposto = 355;
+            var desconto = 0.1;
+
+            double total = preco + imposto;
+            var totalComDesconto = total - total * desconto;
+            Console.WriteLine("O preço final é {0}", totalComDesconto);
+
+            //IMC
+
+            double peso = 62.5;
+            double altura = 1.75;
+            double imc = peso / Math.Pow(altura, 2);
+            Console.WriteLine($"IMC é: {imc}");
+
+            //Par/Impar
+            int par = 24;
+            int impar = 23;
+            Console.WriteLine("{0}/2 tem resto {1}", par, par % 2);
+            Console.WriteLine("{0}/2 tem resto {1}", impar, impar % 2);
+        }
+    }
+}
