687092e1e7c2c123719391057759a18f10fdb30a
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-25 16:12:07 -0300

    Curso C# - Operador Ternário

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -20,6 +20,7 @@ namespace CursoCSharp {
                 {"Operadores Relacionais - Fundamentos", OperadoresRelacionais.Executar},
                 {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar},
                 {"Operadores de Atribuição - Fundamentos", OperadoresDeAtribuicao.Executar},
+                {"Operador Ternário - Fundamentos", OperadorTernario.Executar},
 
             });
 
