e77b13819164965d7321e5d340d09f0843de3180
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-27 13:27:22 -0300

    Curso C# - Estrutura For

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -29,6 +29,7 @@ namespace CursoCSharp {
                 {"Estrutura IF/ELSE/IF - Estruturas de Controle", EstruturaIfElseIf.Executar},
                 {"Estrutura Switch - Estruturas de Controle", EstruturaSwitch.Executar},
                 {"Estrutura While - Estruturas de Controle", EstruturaWhile.Executar},
+                {"Estrutura For - Estruturas de Controle", EstruturaFor.Executar},
 
             });
 
