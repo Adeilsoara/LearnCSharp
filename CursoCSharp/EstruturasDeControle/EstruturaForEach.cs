40d2a0f77adacf57914aacd53e11492536974b47
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-27 13:46:08 -0300

    Curso C# - Estrutura Foreach

---
+++ CursoCSharp/EstruturasDeControle/EstruturaForEach.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturasDeControle {
+    class EstruturaForEach {
+
+        public static void Executar() {
+            var palavra = "Adeilson";
+            foreach (var letra in palavra) {
+                Console.WriteLine(letra);
+            }
+
+            var alunos = new string[] { "Roberto", "Carlos", "Bia" };
+
+            foreach (var aluno in alunos) {
+                Console.WriteLine(aluno);
+
+            }
+        }
+    }
+}
