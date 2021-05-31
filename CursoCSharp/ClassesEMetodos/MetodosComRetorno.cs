59e1fe1cfeb94b76fa5c02c55de590bb80e78602
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-31 15:26:36 -0300

    Curso C# - Metodos com Retorno

--- CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
+++ CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
@@ -10,15 +10,41 @@ namespace CursoCSharp.ClassesEMetodos {
         public int Somar(int a, int b) {
             return a + b;
         }
-
         public int Subtrair(int a, int b) {
             return a + b;
         }
-
         public int Multiplicar(int a, int b) {
             return a * b;
         }
     }
+
+    class CalculadorCadeia {
+        int memoria;
+
+        public CalculadorCadeia Somar(int a) {
+            memoria += a;
+            return this;
+        }
+        public CalculadorCadeia Multiplicar(int a) {
+            memoria *= a;
+            return this;
+        }
+
+        public CalculadorCadeia Limpar() {
+            memoria = 0;
+            return this;
+        }
+
+        public CalculadorCadeia Imprimir() {
+            Console.WriteLine(memoria);
+            return this;
+        }
+
+        public int Resultado() {
+            return memoria;
+        }
+    }
+
     class MetodosComRetorno {
         public static void Executar() {
             var calculadoraComum = new CalculadoraComum();
@@ -27,6 +53,9 @@ namespace CursoCSharp.ClassesEMetodos {
             Console.WriteLine(resultado);
             Console.WriteLine(calculadoraComum.Subtrair(5, 8));
             Console.WriteLine(calculadoraComum.Multiplicar(6,7));
+
+            var calculadoraEncadeada = new CalculadorCadeia();
+            calculadoraEncadeada.Somar(4).Multiplicar(6).Imprimir().Limpar().Resultado();
 
         }
     }
