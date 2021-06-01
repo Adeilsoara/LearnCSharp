09299bedcd91518ab2ad66da4fd56b964cf3dfdc
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-06-01 14:05:10 -0300

    Curso C# - Atributos Estaticos

---
+++ CursoCSharp/ClassesEMetodos/AtributosEstaticos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos {
+
+    public class Produto {
+        public string Nome;
+        public double Preco;
+        public static double Desconto;
+
+        public Produto(string nome, double preco, double desconto) {
+            Nome = nome;
+            Preco = preco;
+            Desconto = desconto;
+        }
+
+        public Produto() {
+
+        }
+
+        public double CalcularDesconto() {
+            return Preco - Preco * Desconto;
+        }
+    }
+    class AtributosEstaticos {
+        public static void Executar() {
+            var produto1 = new Produto("Caneta", 3.2, 0.1);
+
+            Console.WriteLine("Preço com desconto: {0}", produto1.CalcularDesconto());
+        }
+    }
+}
--- CursoCSharp/ClassesEMetodos/MetodosEstaticos.cs
+++ CursoCSharp/ClassesEMetodos/MetodosEstaticos.cs
@@ -24,7 +24,7 @@ namespace CursoCSharp.ClassesEMetodos {
             Console.WriteLine("Resultado: {0}", resultado);
 
             CalculadoraEstatica calc = new CalculadoraEstatica();
-            Console.WriteLine(calc.Somar(2, 2);
+            Console.WriteLine(calc.Somar(2, 2));
 
         }
     }
