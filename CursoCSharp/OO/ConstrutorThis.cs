de2a8635e386277e31200fd3cc94569fa291cacc
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-06-04 10:16:40 -0300

    Curso C# - Construtor This

---
+++ CursoCSharp/OO/ConstrutorThis.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.OO {
+    public class Animal {
+        public string Nome { get; set; }
+
+        public Animal(string nome) {
+            Nome = nome;
+        }
+    }
+    public class Cachorro : Animal {
+        public double Altura { get; set; }
+
+        public Cachorro(string nome) : base(nome) {
+            Console.WriteLine($"Cachorro {nome} instanciado...");
+        }
+
+        public Cachorro(string nome, double altura) : this(nome) {
+            Altura = altura;
+        }
+        public override string ToString() {
+            return $"{Nome} tem {Altura} cm de Altura";
+        }
+    }
+    class ConstrutorThis {
+        public static void Executar() {
+            var leao = new Cachorro("Leão");
+            var joli = new Cachorro("Joli",30.0);
+
+            Console.WriteLine(leao);
+            Console.WriteLine(joli);
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -56,6 +56,7 @@ namespace CursoCSharp {
                 
                   //Orientação a Objetos
                  {"Herança -  OO ", Heranca.Executar},
+                 {"Construtor This -  OO ", ConstrutorThis.Executar},
 
             });
 
