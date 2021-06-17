06052e5a4d2f0395ecf5492e71b3d702d67eaa6a
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-06-17 13:26:58 -0300

    Microservice Part III - Make CRUD Method GetDiscount with Draper

---
+++ Discount.Api/Repositories/DiscountRepository.cs
@@ -0,0 +1,49 @@
+﻿using Dapper;
+using Discount.Api.Entities;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Api.Repositories {
+    public class DiscountRepository : IDiscountRepository {
+
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration) {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        private NpgsqlConnection GetConnectionPostgreSQL() {
+            return new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+
+        public async Task<Coupon> GetDiscount(string productName) {
+            NpgsqlConnection connection = GetConnectionPostgreSQL();
+
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+                ("SELECT * FROM Coupon WHERE ProductName = @ProductName", 
+                new { ProductName = productName});
+
+            if (coupon == null)
+                return new Coupon 
+                { ProductName = "No discount", Amount = 0, Description = "No discount Desc" };
+           
+            return coupon;
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon) {
+            NpgsqlConnection connection = GetConnectionPostgreSQL();
+        }
+
+        public async Task<bool> DeleteDiscount(string productName) {
+            NpgsqlConnection connection = GetConnectionPostgreSQL();
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon) {
+            NpgsqlConnection connection = GetConnectionPostgreSQL();
+        }
+    }
+}
