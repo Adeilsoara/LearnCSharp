86e02a430eea14e6b1e3016465c40eafc7e6afc7
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-06-22 15:10:13 -0300

    Microservice

--- MicroserviceWithPostgres/MicroservicePostgres/Discount.Api/Entities/Coupon.cs
+++ MicroserviceWithPostgres/MicroservicePostgres/Discount.Api/Entities/Coupon.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace Discount.Api.Entities {
     public class Coupon {
-
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
     }
 }
