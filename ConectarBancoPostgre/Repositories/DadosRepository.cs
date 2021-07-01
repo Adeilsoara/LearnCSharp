142bb8dbac3e6504c0683be15e487bf450fbe0c6
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-07-01 15:47:58 -0300

    Microservice - Implementação da interface IdadosRepository na classe repository

---
+++ ConectarBancoPostgre/Repositories/DadosRepository.cs
@@ -0,0 +1,26 @@
+﻿using ConectarBancoPostgre.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConectarBancoPostgre.Repositories {
+    public class DadosRepository : IDadosRepository {
+
+        private readonly IConfiguration _configuration;
+
+        public DadosRepository(IConfiguration configuration) {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public async Task<Dados> GetDados(int gId) {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> CreateDados(Dados dados) {
+            throw new NotImplementedException();
+        }
+
+    }
+}
