9798e2ff25e042e1915e65f6d4c1446fb64332bd
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-07-01 15:35:36 -0300

    Microservice - Adicionando pasta entities e repository

---
+++ ConectarBancoPostgre/Repositories/IDadosRepository.cs
@@ -0,0 +1,12 @@
+﻿using ConectarBancoPostgre.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConectarBancoPostgre.Repositories {
+    public interface IDadosRepository {
+        Task<Dados> GetDados(int gId);
+        Task<bool> CreateDados(Dados dados);
+    }
+}
