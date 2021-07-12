6494358d8a160935fb9b34ea02bc82de3373e38f
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-07-12 10:09:48 -0300

    MicroWorker Test - Criando Pasta Repositories e interface

---
+++ MicroWorkerTest/Repositories/IDadosRepository.cs
@@ -0,0 +1,13 @@
+﻿using MicroWorkerTest.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroWorkerTest.Repositories {
+    public interface IDadosRepository {
+        Task<Dados> GetDados(int gId);
+        Task<List<Dados>> GetDadosAll();
+        Task<bool> CreateDados(Dados dados);
+    }
+}
