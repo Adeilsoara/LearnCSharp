b2ec26b9ec50d486d7a2abba0580afaa28ca1619
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-07-12 10:06:41 -0300

    MicroWorker Test - Criando Pasta Entities e classe Dados

---
+++ MicroWorkerTest/Entities/Dados.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroWorkerTest.Entities {
+    public class Dados {
+        public int GId { get; set; }
+        public string Endereco { get; set; }
+    }
+}
