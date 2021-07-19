42e9f6c482ac574541299797ebeef72bdfda1241
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-07-19 14:04:06 -0300

    MicroWorkerLocal - Configurando interfaces e classes de consultas

---
+++ MicroWorkerLocal/Repositories/IDadosRepository.cs
@@ -0,0 +1,13 @@
+﻿using MicroWorkerLocal.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroWorkerLocal.Repositories {
+    public interface IDadosRepository {
+        Task<Dados> GetDados(int gId);
+        Task<List<Dados>> GetDadosAll();
+        Task<bool> CreateDados(Dados dados);
+    }
+}
