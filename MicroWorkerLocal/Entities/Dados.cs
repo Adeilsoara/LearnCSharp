c1888dcccd103daa573af1d465a997d55dd3da88
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-07-19 15:11:32 -0300

    MicroWorkerLocal - Suporte ao docker

--- MicroWorkerLocal/Entities/Dados.cs
+++ MicroWorkerLocal/Entities/Dados.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Spatial;
 using System.Threading.Tasks;
 
 namespace MicroWorkerLocal.Entities {
     public class Dados {
         public int GId { get; set; }
         public string Endereco { get; set; }
+        public Geometry Geom { get; set; }
     }
 }
