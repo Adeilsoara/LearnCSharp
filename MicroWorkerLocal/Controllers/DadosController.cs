597e16adab04943ab10d0f8523126242935daf2f
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-07-19 14:12:10 -0300

    MicroWorkerLocal - Configurando rotas e verbos http

---
+++ MicroWorkerLocal/Controllers/DadosController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MicroWorkerLocal.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroWorkerLocal.Controllers {
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DadosController : ControllerBase {
+        private readonly IDadosRepository _repository;
+
+        public DadosController(IDadosRepository repository) {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [Route("dados")]
+        public async Task<IActionResult> GetDadosAll() {
+            var result = await _repository.GetDadosAll();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("dado")]
+        public async Task<IActionResult> GetDados(int gId) {
+            var result = await _repository.GetDados(gId);
+            return Ok(result);
+        }
+    }
+}
--- MicroWorkerLocal/Repositories/DadosRepository.cs
+++ MicroWorkerLocal/Repositories/DadosRepository.cs
@@ -1,9 +1,44 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using MicroWorkerLocal.Entities;
+using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MicroWorkerLocal.Repositories {
-    public class DadosRepository {
+    public class DadosRepository : IDadosRepository {
+
+        private readonly IConfiguration _configuration;
+
+        public DadosRepository(IConfiguration configuration) {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        private NpgsqlConnection GetConnectionStringSQL() {
+
+            return new NpgsqlConnection(_configuration.GetValue<string>
+                ("DatabaseSettings:ConnectionString"));
+        }
+
+        public Task<bool> CreateDados(Dados dados) {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Dados> GetDados(int gId) {
+            NpgsqlConnection connection = GetConnectionStringSQL();
+            string query = "SELECT * FROM ponto_importado_corrigido WHERE gid = @gId";
+            Dados dados = await connection.QueryFirstOrDefaultAsync<Dados>(sql: query, param: new { gId });
+
+            return dados;
+        }
+
+        public async Task<List<Dados>> GetDadosAll() {
+            NpgsqlConnection connection = GetConnectionStringSQL();
+            string query = "SELECT gid, endereco FROM ponto_importado_corrigido LIMIT 5; ";
+            List<Dados> lista = (await connection.QueryAsync<Dados>(sql: query)).ToList();
+            return lista;
+        }
     }
 }
