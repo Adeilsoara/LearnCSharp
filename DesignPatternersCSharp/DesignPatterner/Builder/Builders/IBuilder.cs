a18d5ee13bd71cf46034d3c1660eca59c6a5c998
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-08-11 14:55:16 -0300

    Design Patterners - Builder

---
+++ DesignPatternersCSharp/DesignPatterner/Builder/Builders/IBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Builder.Components;
+using Builder.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.Builders {
+    interface IBuilder {
+        void Reset();
+        Vehicle GetVehicle();
+        void SetSeats(int seats);
+        void SetEngine(Engine engine);
+        void SetTramission(Tramission tramission);
+        void SetVehicleType(VehicleType vehicleType);
+    }
+}
---
+++ DesignPatternersCSharp/DesignPatterner/Builder/Products/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using Builder.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.Products {
+    class Vehicle {
+        private VehicleType vehicleType;
+        private int seats;
+        private Engine engine;
+        private Tramission tramission;
+
+        public VehicleType VehicleType {
+            get => vehicleType;
+            set => vehicleType = value;
+        }
+        public int Seats {
+            get => seats;
+            set => seats = value;
+        }
+
+        public Engine Engine {
+            get => engine;
+            set => engine = value;
+        }
+        public Tramission Tramission {
+            get => tramission;
+            set => tramission = value;
+        }
+
+    }
+}
