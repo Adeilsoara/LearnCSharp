fe9329650e61f18c1a954fc1ab4c51a2cf834b23
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-08-13 10:55:38 -0300

    Monitoring Data Modifications With CSharp and Postgresql

---
+++ MonitoringDataBase/MonitoringDataBase/Program.cs
@@ -0,0 +1,21 @@
+﻿using Npgsql;
+using System;
+using System.Threading.Tasks;
+
+namespace MonitoringDataBase {
+    class Program {
+        static async Task Main(string[] args) {
+            const string connString = "Server=localhost;Port=5432;Database=teste;User Id=postgres;Password=admin;";
+            await using var conn = new NpgsqlConnection(connString);
+            await conn.OpenAsync();
+
+            conn.Notification += (o, e) => Console.WriteLine("Receiveid notification:" + e.Payload);
+
+            await using (var cmd = new NpgsqlCommand("Listen datachange;", conn)) 
+                cmd.ExecuteNonQuery();
+
+            while (true)
+                conn.Wait();
+        }
+    }
+}
